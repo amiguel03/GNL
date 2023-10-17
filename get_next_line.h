c5303e75487aa07ade1c6572cae23099fc2528bb
amiguel03 <148122874+amiguel03@users.noreply.github.com>
2023-10-17 12:29:41 +0200

    Add files via upload

---
+++ get_next_line.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/16 09:30:08 by marvin            #+#    #+#             */
+/*   Updated: 2023/10/16 09:30:08 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include<unistd.h>
+# include<stdio.h>
+# include<stdlib.h>
+# include<fcntl.h>
+# include<limits.h>
+
+size_t	ft_strlcpy(char *dst, char *src, size_t dstsize);
+size_t	ft_strlen(char *s);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strdup(char *s1);
+char	*ft_strchr(char *s, int c);
+char	*ft_substr(char *s, unsigned int start, size_t len);
+char	*get_next_line(int fd);
+char	*ft_free(char **str);
+#endif
